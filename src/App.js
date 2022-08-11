e8505ffbb3e7fb92c9e51cd08b9b8b83372d1e1c
Temitope Hassan <temitope.hassan.2@gmail.com>
2022-08-11 19:49:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import style from './App.module.scss';
 
 // Defaults to 6 days, 22 hours, 40 minutes and 0 seconds from now in your timezone.
 const futureDate = add(new Date(), {
-  days: 6,
-  hours: 22,
+  days: 13,
+  hours: 4,
   minutes: 40
 });
 
@@ -19,8 +19,8 @@ function App() {
 
   return (
     <div className={ style.pageContainer }>
-      <h1>Join Our Event</h1>
-      <p>Come check out our great event. It will be lots of fun.</p>
+      <h2>Northern Coalition For Peter Obi</h2>
+      <p>Site In Development</p>
       { tickerEl }
       <button 
         className={ style.toggleButton } 
